7a6e4134ac80670d94974d1908a0f7a0bab6cb76
willian-virgilio <willian.virgilio@gmail.com>
2022-07-15 21:36:14 -0300

    Adicionada Flake 8

---
+++ libpythonpro/Readme.md
@@ -0,0 +1,7 @@
+Para instalar: 
+""" console
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements-dev.txt
+
+"""
---
+++ libpythonpro/github.api.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def buscar_avatar():
+    """
+    Busca o avatar de ym usuário no Github
+    :param usuario: srt com nome do usuario
+    :return:str com o link do avatar
+    """
+    url = f'https://api.github.com/users/willian-virgilio'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar())
